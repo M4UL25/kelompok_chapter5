7d05594fef55925c31324a3a24ec83d2e5717a1b
unknown <sastraharapangulo2002@gmail.com>
2022-10-06 19:27:04 +0700

    update 1.2

---
+++ public/script/script.js
@@ -0,0 +1,10 @@
+var toggler = document.getElementById("toggler");
+var logo = document.getElementById("logo");
+var sidebar = document.getElementById("sidebar");
+var pagecontent = document.getElementById("page-content");
+
+toggler.addEventListener("click", (e) => {
+    sidebar.classList.toggle("hidden");
+    logo.classList.toggle("hidden");
+    pagecontent.classList.toggle("marginleft");
+});
