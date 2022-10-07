d4537f14cd2a8a429495350cdc58266426b63507
unknown <sastraharapangulo2002@gmail.com>
2022-10-07 12:28:08 +0700

    filter

--- public/script/script.js
+++ public/script/script.js
@@ -9,3 +9,35 @@ toggler.addEventListener("click", (e) => {
     pagecontent.classList.toggle("marginleft");
 });
 
+function filter(value){
+    let buttons = document.querySelectorAll(".button-option");
+    buttons.forEach(button => {
+        if(value.toUpperCase() == button.innerText.toUpperCase()){
+            button.classList.add("active");
+        }
+        else{
+            button.classList.remove("active");
+        } 
+    });
+    
+    let cards = document.querySelectorAll(".card");
+    let ukuran = document.querySelectorAll(".ukuran");
+    let i;
+
+    for(i=0;i<cards.length;i++){
+        if(value == "all"){
+            cards[i].classList.remove("hide");
+        }
+        else{
+            if(ukuran[i].innerHTML.toUpperCase() != value.toUpperCase()){
+                cards[i].classList.add("hide");
+            }
+            else{
+                cards[i].classList.remove("hide");
+            }
+        }
+        
+    }
+}
+
+
